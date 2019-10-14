70eede111678b4f93298abff38364cb402cf06e1
S.F <1074260090@qq.com>
2019-10-14 17:20:13 +0800

    不让我爬

--- 多线程/imgs.py
+++ 多线程/imgs.py
@@ -1,3 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
 def get_page_urls():
     for i in range(1,212):
-        baseurl = 'https://www.mzitu.com/page/{}'.format(i)
+        # baseurl = 'https://www.mzitu.com/page/{}'.format(i)
+        baseurl = 'https://www.mzitu.com/page/1'
+        html = request_page(baseurl)
+        soup = BeautifulSoup(html)
+        list = soup.find(class_='postlist').find_all('li')
+        urls = []
+        for item in list:
+            url = item.find('span').find('a').get('href')
+            urls.append(url)
+    print(urls)
+    return urls
+def request_page(url):
+    try:
+        response = requests.get(url)
+        print(response.text)
+        if response.status_code == 200:
+            return response.text
+    except requests.RequestException:
+        return None
+get_page_urls()
